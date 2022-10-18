3b27d80e5b8d76d97b76e4ba71eff96c13698e46
anandmahamuni <m.anand7203@gmail.com>
2022-10-18 22:46:40 +0530

    Problem A9

--- FDSL/codedumps/A9.py
+++ FDSL/codedumps/A9.py
@@ -1,7 +1,8 @@
 m=int(input("Enter the number of rows="))
 n=int(input("Enter the number of columns="))
 
-mata,matb,matadd,matat,matbtr=[],[],[],[],[]
+mult=0
+mata,matb,matadd,matsubt,matmul,matat,matbtr=[],[],[],[],[],[],[]
 
 print("For the 1st Matrix:-")
 
@@ -21,16 +22,22 @@ for k in range(0,m):
         colb.append(b)
     matb.append(colb)
 
-print("For the Addition of the Matrices:-")
-
 for g in range(0,m):
     coladd=[]
+    colsubt=[]
     for h in range(0,n):
         add=mata[g][h]+matb[g][h]
+        subt=mata[g][h]-matb[g][h]
         coladd.append(add)
+        colsubt.append(subt)
     matadd.append(coladd)
+    matsubt.append(colsubt)    
 
+
+print("For the Addition of the Matrices:-")
 print(matadd)
+print("For the Subtraction of the Matrices:-")
+print(matsubt)
 
 print("For the Transpose of the both Matrices:-")
 
@@ -47,7 +54,19 @@ for p in range(0,m):
     colbtr=[]
     for q in range(0,n):
         t2=matb[q][p]
-        colat.append(t2)
+        colbtr.append(t2)
     matbtr.append(colbtr)
 
 print(matbtr)
+
+print("For the Multiplication of the both Matrices:-\n")
+
+for u in range(0,m):
+    colmul=[]
+    for v in range(len(matb[0])):
+        for w in range(len(matb)):
+            mult+= mata[u][v] * matb[w][v]
+        colmul.append(mult)
+    matmul.append(colmul)
+
+print(matmul)
